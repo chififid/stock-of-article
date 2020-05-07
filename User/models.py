d2ad567d23502ae096dae0345e956e40fbc3558f
chififid <57342377+chififid@users.noreply.github.com>
2020-05-07 12:36:22 +0300

    Add times limits for registration

--- User/models.py
+++ User/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from .managers import CustomUserManager
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from datetime import datetime, timedelta
 
 class User(AbstractUser):
     username = models.CharField(_('username'), max_length=150, blank=True)
@@ -23,9 +24,15 @@ class Activate(models.Model):
     key = models.IntegerField(unique=True)
     activate = models.BooleanField(null=True, blank=True, default=False)
     password = models.CharField(max_length=150, blank=True, null=True)
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
 
     def __str__(self):
-        return str(self.key)
+        a = datetime.now()
+        b = timedelta(minutes=20)
+        if a.replace(tzinfo=None) - self.published.replace(tzinfo=None) >= b:
+            self.delete()
+        else:
+            return str(self.key)
 
     class Meta:
         verbose_name = 'Активация'
